65bd70d37b6fae0cdb943425b0694d903bce6f55
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-01-08 10:59:43 +0000

    Update samyuktha33.C

--- samyuktha33.C
+++ samyuktha33.C
@@ -1,14 +1,21 @@
 #include<stdio.h>
+#include<string.h>
 void main()
 {
-    char a[1000];
-    int b=0;
-    printf("\n Enter string");
-    scanf("%s",&a);
-    if(a[b]%1==0 && b!="\0")
-    {
-        b++;
-        
-    }
-printf("%d",b);
+char a[1000];
+int b = 0, c= 0, d = 0;
+puts("Enter any string\n");
+gets(a);
+for(b= 0; a[b] !='\0'; b++)
+{
+c = c + 1;
+}
+for(b = 0; b <= c-1; b++)
+{
+if(a[b] == ' ')
+{
+d = d + 1;
+}
+}
+printf("The number of space in the line are %d", d);
 }
